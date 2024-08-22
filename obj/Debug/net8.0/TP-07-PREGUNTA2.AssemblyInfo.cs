96b6e66008c82ef3f78aa4d7a3868437f25477e8
IairSocha <48240876@est.ort.edu.ar>
2024-08-22 16:00:52 -0300

    unas cositas más

--- obj/Debug/net8.0/TP-07-PREGUNTA2.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP-07-PREGUNTA2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP-07-PREGUNTA2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+673074a303f32d7b425c3e5cdc784679c37645de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de5245da8577526f0d3b575b4fdff93a37161f60")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP-07-PREGUNTA2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP-07-PREGUNTA2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
