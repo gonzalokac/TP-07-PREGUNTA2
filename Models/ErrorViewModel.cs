f7c6de7fdb73057665b84b57ec7721573da9d9b9
IairSocha <48240876@est.ort.edu.ar>
2024-08-22 13:50:18 -0300

    mvc hecho

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP_07_PREGUNTA2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
